b61161b320362a0d5b7a6ef0c28424d3baa64165
Doshant <doshant54rahangdale@gmail.com>
2024-09-27 22:47:48 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Navbar.css';
-import logo from './Logo2.png'
+import logo from './Logo2.png';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/'); // Navigates to the home page
+  };
+
   return (
     <div className="navbar">
-      <img src={logo} alt="Logo" className="logo" /> {/* Add logo image */}
+      <img 
+        src={logo} 
+        alt="Logo" 
+        className="logo" 
+        onClick={handleLogoClick} // Add onClick handler to navigate
+        style={{ cursor: 'pointer' }} // Change cursor to pointer to indicate it's clickable
+      />
       <h1>Pharma Covigilance Platform</h1>
     </div>
   );
